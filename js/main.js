0e72e867b394fcba695a1b4d9d63a4b4f9407638
MoRahh <moodyaburahma@gmail.com>
2024-01-08 02:36:56 +0300

    added more content

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+// Select tab content item
+function selectItem(e) {
+    // Remove all show and border classes
+    removeBorder();
+    removeShow();
+    // Add border to current tab item
+    this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class
+    tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border');
+    });
+}
+
+// Remove show class from all content items
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show');
+    });
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+});
+
+
+
+// FAQ Section
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+
+        var pannel = this.nextElementSibling;
+
+        if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+        } else {
+            pannel.style.display = "block";
+        }
+    });
+}
+
+
+
+
+
